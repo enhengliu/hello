c11e0ed8fcb5c7ffe3ec46e4a1f1a15a832b36fc
AndyLiu <andy_liu@nox.con.tw>
2019-11-22 11:59:36 +0800

    modify

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/andyliu/Library/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/andyliu/Flutter/hello"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/andyliu/Flutter/hello/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/andyliu/Library/flutter/bin/cache/artifacts/engine/ios"
